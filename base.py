40703934fd1e8837cc453f63671d93170eaf85f8
Anssi Kääriäinen <akaariai@gmail.com>
2012-11-13 11:38:47 +0200

    Works on git::github.com/akaariai/django.git@ticket_19274

--- base.py
+++ base.py
@@ -1,10 +1,11 @@
+import copy
 import threading
 
-from django.db.utils import load_backend, DatabaseError
+from django.db.utils import load_backend
 
 class PoolKey(object):
     def __init__(self, alias, settings):
-        self.key = (alias, settings['NAME'])
+        self.key = (alias, settings['NAME'], settings['USER'])
 
     def __eq__(self, other):
         return isinstance(other, self.__class__) and self.key == other.key
@@ -28,22 +29,31 @@ class Pool(object):
     pools = {}
     lock = threading.Lock()
 
+    def _status(self):
+        for key, conns in self.pools.items():
+            if len([c for c in conns if c.in_use]) >= 0:
+                print(key, len([c for c in conns if c.in_use]))
+            for conn in conns:
+                print(conn, conn.in_use, conn.abandoned)
+
     def acquire_connection(self, key):
         conn = None
         while conn is None:
             with self.lock:
-                conn = self._acquire_connection(key)
+                conn = self.acquire_connection_inner(key)
                 if conn is None:
                     # No available connections
                     return None
-            if not self.verify_connection(conn):
-                with self.lock:
+                try:
+                    self.verify_connection(conn)
+                except:
+                    conn.abandoned = True
                     self.pools[key].remove(conn)
-                conn = None
+                    conn = None
+                    raise
         return conn
-    
-    def _acquire_connection(self, key):
-        # self.lock is assumed to be locked by this thread
+
+    def acquire_connection_inner(self, key):
         try:
             for conn in self.pools[key]:
                 if conn.in_use:
@@ -59,40 +69,41 @@ class Pool(object):
             self.pools[conn.key].append(conn)
 
     def verify_connection(self, conn):
-        try:
+        if conn.on_connect:
             cursor = conn.connection.cursor()
             cursor.execute(conn.on_connect)
             cursor.fetchone()
-            return True
-        except:
-            # log - return False
-            raise
 
     def release_connection(self, conn):
         if conn.abandoned:
+            assert not conn.in_use
             return
         try:
-            try:
-                conn.connection.rollback()
-            except:
-                pass
-            conn.in_use = False
-        except DatabaseError, e:
-            with self.lock:
-                self.pools[conn.key].remove(conn)
-            # logger.log...
-                
+            conn.connection.rollback()
+        except:
+            # Uqly, but there is no common exception class for different
+            # DB adapters. At least logging would be good here...
+            pass
+        conn.in_use = False
+
     def close_all(self):
         with self.lock:
             for conns in self.pools.values():
                 for conn in conns[:]:
                     conn.connection.close()
-                    conn.abandoned = True
                     conns.remove(conn)
-
-pool = Pool() 
+# Global pool.
+pool = Pool()
 
 class PoolReleaser(object):
+    """
+    Somewhat ugly situation: we must release the connection on garbage
+    collection back to the pool. However, if there is a reference cycle then
+    __del__ is not called for the object (actually, the object is not ever
+    GCed). So, this class is there only to break the reference cycle so that
+    GC will work, yet __del__ is called. This is ugly but seems to work. Better
+    ideas are very much welcome :)
+    """
     def __init__(self):
         self.pool_object = None
         # Forbid GC of the pool before connections are cleaned.
@@ -101,57 +112,79 @@ class PoolReleaser(object):
     def release(self):
         if self.pool_object:
              self.pool.release_connection(self.pool_object)
+             self.pool_object = None
 
     def __del__(self):
         # Trick: when the DBWrapper below is carbage collected, I will get
         # called. Hopefully.
-        self.release()
+        if self.pool_object:
+             self.release()
 
 class WrappedCreation(object):
+    """
+    Need to wrap the real creation object so that it is possible to destroy
+    the test database after tests.
+    """
     def __init__(self, real_creation):
         self.real_creation = real_creation
 
     def __getattr__(self, attr):
         return getattr(self.real_creation, attr)
-    
+
     def destroy_test_db(self, *args, **kwargs):
+        # Destroying a db isn't possible if there are still connections open
+        # to the test db. So, close all pool connection before trying to
+        # destroy the DB.
         pool.close_all()
         self.real_creation.destroy_test_db(*args, **kwargs)
 
+# wrapped engine -> dynamic wrapper
+dyn_wrap_cache = {}
 def DatabaseWrapper(settings, *args, **kwargs):
     """
-    This is just evil - the caller thinks he is instantiating base.DatabaseWrapper.
-    But not true! He is actually calling this factory method.
+    This is just evil - the caller thinks he is instantiating
+    base.DatabaseWrapper. But not true! He is actually calling this
+    factory method - we are pretending to be a class here. Hopefully nobody
+    is doing isinstance checks against DatabaseWrapper... :)
+
+    Returns a dynamically created wrapper for the settings.wrapped connection.
     """
-    settings = settings.copy()
-    settings['ENGINE'] = wraps =  settings['OPTIONS'].pop('WRAPS')
+    settings = copy.deepcopy(settings)
+    settings['ENGINE'] = wraps = settings['OPTIONS'].pop('WRAPS', None)
     if wraps is None:
         raise RuntimeError('You must define OPTIONS["WRAPS"] in settings '
                            'for alias %s.' % args[0])
-    dbwrapper = load_backend(wraps).DatabaseWrapper
     pool_opts = dict(ON_CONNECT=settings['OPTIONS'].pop('ON_CONNECT', 'select 1'))
 
-    def _cursor(self):
-        key = PoolKey(self.alias, self.settings_dict)
-        if not self.connection:
+    if wraps in dyn_wrap_cache:
+        dynwrap = dyn_wrap_cache[wraps]
+    else:
+        dbwrapper = load_backend(wraps).DatabaseWrapper
+        # Methods we are going to add to the dynamically created wrapper.
+        def get_new_connection(self, conn_params):
+            key = PoolKey(self.alias, self.settings_dict)
             pooled = pool.acquire_connection(key)
             if pooled:
-                self.connection = pooled.connection
-                return super(self.__class__, self)._cursor()
-            cursor = super(self.__class__, self)._cursor()
-            self.pool_releaser.pool_object = PoolObject(key, self.connection, self.pool_opts) 
+                self.pool_releaser.pool_object = pooled
+                return pooled.connection
+            conn = super(self.own_class, self).get_new_connection(conn_params)
+            self.pool_releaser.pool_object = PoolObject(key, conn,
+                                                        self.pool_opts)
             pool.add_connection(self.pool_releaser.pool_object)
-            return cursor
-        else:
-            assert key == self.pool_releaser.pool_object.key
-            return super(self.__class__, self)._cursor()
-    
-    def close(self):
-        self.connection = None
-        self.pool_releaser.release()
-    
-    dynwrap = type('Pooled' + dbwrapper.__name__,
-                  (dbwrapper,), {'_cursor': _cursor, 'close': close, 'pool_opts': pool_opts})
+            return conn
+
+        def close(self):
+            self.validate_thread_sharing()
+            if self.connection:
+                self.connection = None
+                self.pool_releaser.release()
+
+        dynwrap = type('Pooled' + dbwrapper.__name__,
+                      (dbwrapper,), {'get_new_connection': get_new_connection,
+                                     'close': close,
+                                     'pool_opts': pool_opts})
+        dynwrap.own_class = dynwrap
+        dyn_wrap_cache[wraps] = dynwrap
     conn = dynwrap(settings, *args, **kwargs)
     conn.pool_releaser = PoolReleaser()
     conn.creation = WrappedCreation(conn.creation)
